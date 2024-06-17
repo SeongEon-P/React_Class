489404b1bf15235c2991a213566c5e4d0be4afb5
박성언 <tjddjs909@naver.com>
2024-06-17 11:15:03 +0900

    UserServiceImpl에서 제일 끝부분 newRole에서 오류발생. 일단 newRole -> newRole.name()이렇게 해서 오류 없애긴했는데, 근본적인 해결인지는 모르겠음.

---
+++ ShopBackend/src/main/java/com/example/shopbackend/service/UserService.java
@@ -0,0 +1,12 @@
+package com.example.shopbackend.service;
+
+import com.example.shopbackend.model.Role;
+import com.example.shopbackend.model.User;
+
+import java.util.Optional;
+
+public interface UserService {
+    User saveUser(User user);
+    Optional<User> findByUsername(String username);
+    void changeRole(Role newRole, String username);
+}
