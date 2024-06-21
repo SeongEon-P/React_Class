0a0970b0531213e4928465974b06ca71d0c1bea9
박성언 <tjddjs909@naver.com>
2024-06-21 14:33:24 +0900

    @AuthenticationPrincipal은 로그인한 사용자 정보를 담는 역할, role 변경하는 코드

--- ShopBackend/src/main/java/com/example/shopbackend/controller/UserController.java
+++ ShopBackend/src/main/java/com/example/shopbackend/controller/UserController.java
@@ -1,40 +1,23 @@
 package com.example.shopbackend.controller;
 
-import com.example.shopbackend.model.User;
+import com.example.shopbackend.model.Role;
+import com.example.shopbackend.security.UserPrinciple;
 import com.example.shopbackend.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-
 @RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @GetMapping("/home")
-    public String Home() {
-        return "Hello World";
-    }
-
-    @PostMapping("/user")
-    public User newUser(@RequestBody User newUser) {
-        return userService.saveUser(newUser);
-    }
-
-    @GetMapping("/users")
-    public List<User> findAllUsers() {
-        return userService.findAll();
-    }
-
-    @GetMapping("/user/{username}")
-    public Optional<User> getUserByUsername(@PathVariable String username) {
-        return userService.findByUsername(username);
-    }
-
-    @PutMapping("user")
-    public void changeRole(@RequestBody User user){
-        userService.changeRole(user.getRole(), user.getUsername());
+    @PutMapping("change/{role}") //수정하는 거라서 PutMapping
+    public ResponseEntity<Object> changeRole(@AuthenticationPrincipal UserPrinciple userPrinciple,
+                                             @PathVariable Role role) {
+        userService.changeRole(role, userPrinciple.getUsername());
+        return ResponseEntity.ok(true);
     }
 }
