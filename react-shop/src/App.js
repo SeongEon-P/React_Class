8a7bb51cd6f2e5fa71b15d7350b41d382dd051b5
박성언 <tjddjs909@naver.com>
2024-06-24 17:04:38 +0900

    실행했을때 회원가입 화면에서 이름 입력칸 밖에 안뜸. 유저네임, 패스워드 추가해야함.

--- react-shop/src/App.js
+++ react-shop/src/App.js
@@ -1,32 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-// import Home from './pages/home/home';
-// import Login from './pages/login/login';
-// import Register from './pages/register/register';
-// import Admin from './pages/admin/admin';
-// import NotFound from './pages/404/404';
-// import UnAuthorized from './pages/401/401';
-// import NotFound from './pages/*/*';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Admin from './pages/admin/Admin';
+import NotFound from './pages/not-found/NotFound';
+import UnAuthorized from './pages/unauthorized/UnAuthorized';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <BrowserRouter> */}
+      <BrowserRouter>
         <Navbar />
         <div className="container">
-          {/* <Routes> */}
-            {/* <Route path='/' element={<Home />}></Route> */}
-            {/* <Route path='/home' element={<Home />}></Route>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/home' element={<Home />}></Route>
             <Route path='/login' element={<Login />}></Route>
-            <Route path='/register' element={<Register />}></Route> */}
-            {/* <Route path='/admin' element={<Admin />}></Route> */}
-            {/* <Route path='/404' element={<NotFound />}></Route>
+            <Route path='/register' element={<Register />}></Route>
+            <Route path='/admin' element={<Admin />}></Route>
+            <Route path='/404' element={<NotFound />}></Route>
             <Route path='/401' element={<UnAuthorized />}></Route>
-            <Route path='/*' element={<NotFound />}></Route> */}
-          {/* </Routes> */}
+            <Route path='/*' element={<NotFound />}></Route>
+          </Routes>
         </div>
-      {/* </BrowserRouter> */}
+      </BrowserRouter>
     </div>
   );
 }
---
+++ react-shop/src/store/configStore.js
@@ -0,0 +1,10 @@
+import {combineReducers, legacy_createStore as createStore} from 'redux'
+import userReducer from './reduces/user'
+
+const allReducers=combineReducers({
+    user:userReducer
+})
+
+const store=createStore(allReducers);
+
+export default store;
