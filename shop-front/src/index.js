289bf28a3b94bb5495b13635527f602ec0d83b56
박성언 <tjddjs909@naver.com>
2024-07-05 09:46:01 +0900

    실행시키고, 로그인 하면 403에러가 떴었는데, PurchaseController에서 GetMapping 옆에 ("/getList")를 지우면 됬음. VScode에서 purchase.service.js에서 getAllPerchase에 API_URL 옆에 +"/getList"를 추가해도됨. 어쨋든 서버랑 맞춰줘야함

--- shop-front/src/index.js
+++ shop-front/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
-import store from './store/configStore';
 import {Provider} from 'react-redux';
+import {store, persistor} from './store/configStore';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- shop-front/src/store/configStore.js
+++ shop-front/src/store/configStore.js
@@ -1,10 +1,22 @@
-import {combineReducers, legacy_createStore as createStroe} from 'redux'
-import userReducer from './reduces/user'
+import {combineReducers, legacy_createStore as createStore} from 'redux'
+import {persistStore, persistReducer} from 'redux-persist';
+//Redux Persists에서 사용할 스토리지 엔진(로컬 스토리지 사용)
+import storage from 'redux-persist/lib/storage';
+
+import userReducer from './reduces/user';
+
+const persistConfig={
+    key:'root', // 로컬 스토리지에 저장되는 키
+    storage,
+}
 
 const allReducers=combineReducers({
     user:userReducer
 })
 
-const store=createStroe(allReducers);
+//persistConfig에 따라 상태를 로컬 스토리지에 저장하고 관리.
+const persistedReducer = persistReducer(persistConfig, allReducers);
+const store = createStore(persistedReducer); // persistedReducer 이용해서 지속성 유지한다
+const persistor = persistStore(store); //지속성 객체를 만든다.
 
-export default store;
+export {store, persistor};
