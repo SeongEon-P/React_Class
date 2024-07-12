56ffdc89b4b3455a962e8573eb82b564d213cc63
박성언 <tjddjs909@naver.com>
2024-07-12 14:45:25 +0900

    상세보기 페이지에서 영화 트레일러 들고오는걸로 수정 완료

--- movie-app/src/components/SingleMovie/SingleMovie.jsx
+++ movie-app/src/components/SingleMovie/SingleMovie.jsx
@@ -3,10 +3,10 @@ import { useParams } from "react-router-dom";
 import Star from '../../assets/star.png';
 import "./SingleMovie.css";
 
-const SingleMovie=()=>{
+const SingleMovie = () => {
     const { movieId } = useParams();
-    const [movie, setMovie]=useState("");
-    const [genres, setGenres]=useState([]);
+    const [movie, setMovie] = useState("");
+    const [genres, setGenres] = useState([]);
     const [trailerKey, setTrailerKey] = useState(""); // trailerKey 상태 정의
 
     useEffect(() => {
@@ -35,42 +35,19 @@ const SingleMovie=()=>{
         }
     };
 
-
-
     if (!movie) {
         return <div>Loading...</div>;
     }
 
-    return(
+    return (
         <div className="single-movie">
-            <button className="back-button" onClick={() => window.history.back()}>목록으로 돌아가기</button>
             <div className="movie-details-container">
-                <img
-                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                    alt="movie poster"
-                    className="movie_poster"
-                />
-                <div className="movie-info">
-                    <div>영화 아이디 : {movie.id}</div><br/>
-                    <div>영화 제목 : {movie.title}</div><br/>
-                    <div>original 제목 : {movie.original_title}</div><br/>
-                    <div>overview : <p>{movie.overview}</p></div><br/>
-                    <p>개봉일 : {movie.release_date}</p><br/>
-                    <p className="align_center">
-                        평점 : {movie.vote_average}
-                        <img src={Star} alt="rating icon" className="card_emoji" />
-                    </p><br/>
-                    <p>장르 : 
-                        {genres.map((genre, idx) =>
-                            <span key={idx}>{genre.name} | </span>
-                        )}
-                    </p><br/>
-                    {trailerKey && (
+                {trailerKey && (
+                    <>
                         <div className="trailer">
-                            <h3>영화 트레일러</h3>
                             <iframe
-                                width="560"
-                                height="315"
+                                width="100%"
+                                height="500"
                                 src={`https://www.youtube.com/embed/${trailerKey}?autoplay=1`}
                                 title="YouTube video player"
                                 frameBorder="0"
@@ -78,8 +55,27 @@ const SingleMovie=()=>{
                                 allowFullScreen
                             ></iframe>
                         </div>
-                    )}
-                </div>
+                        <div className="movie-info">
+                            <div className="movie-title">영화 제목 
+                            <br/>
+                            {movie.title}</div><br/>
+                            <div>줄거리
+                            <br/>
+                            <p>{movie.overview}</p></div><br/>
+                            <p>개봉일 : {movie.release_date}</p><br/>
+                            <p className="align_center">
+                                평점 : {movie.vote_average}
+                                <img src={Star} alt="rating icon" className="card_emoji" />
+                            </p><br/>
+                            <p>장르 : 
+                                {genres.map((genre, idx) =>
+                                    <span key={idx}>{genre.name} | </span>
+                                )}
+                            </p><br/>
+                            <button className="back-button" onClick={() => window.history.back()}>목록으로 돌아가기</button>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     );
